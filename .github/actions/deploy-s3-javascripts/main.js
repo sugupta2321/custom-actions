4b9e97ef38beb8aeb5eb5384f846be04e400a3e4
Sudhanshu Gupta <sugupta2321@gmail.com>
2025-12-29 17:45:01 +0530

    javascript custom action

---
+++ .github/actions/deploy-s3-javascripts/main.js
@@ -0,0 +1,8 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
+function run() {
+    core.notice('Deploying JavaScripts to S3 bucket')
+}
+
+run()
